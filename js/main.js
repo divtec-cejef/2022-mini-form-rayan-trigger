12f9494d85c9ba233f2c4cc03ab414a96b75e8cf
Rondez Rayan <rayan.rondez@divtec.ch>
2022-09-13 11:06:01 +0200

    commit fin c'est finito

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@
 'use strict';
 
 console.log('Test liaison JS');
+const formulaire = document.querySelector('form')
+formulaire.addEventListener('submit', function(event) {
+    event.preventDefault();//Annule le comportement par défault (submit)
+    alert('Envoyé !');
+})
+
+//Au changement de la valeur de la liste
+const background = document.getElementById('background')
+background.addEventListener('change', function(event) {
+    event.preventDefault();//Annule le comportement par défault (submit)
+    document.body.style.backgroundImage = `url(./img/backgrounds/${background.value})`
+})
