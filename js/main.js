5925d9be3d35fdb946900e47bcfe99d66fbdcf13
Rondez Rayan <rayan.rondez@divtec.ch>
2022-09-20 08:50:56 +0200

    fin

--- js/main.js
+++ js/main.js
@@ -10,11 +10,20 @@ const formulaire = document.querySelector('form')
 formulaire.addEventListener('submit', function(event) {
     event.preventDefault();//Annule le comportement par défault (submit)
     alert('Envoyé !');
-})
+    formulaire.reset();
+    btCreer.classList.remove(`animate__heartBeat`);
+    // Récupérer le bouton
+    const btCreer = document.querySelector(`[type="submit"]`);
+    // Animer le bouton
+    btCreer.classList.add(`animate__heartBeat`);
+    btCreer.addEventListener(`animationend`, function () {
+    });
+});
 
 //Au changement de la valeur de la liste
-const background = document.getElementById('background')
+const background = document.getElementById('background');
 background.addEventListener('change', function(event) {
     event.preventDefault();//Annule le comportement par défault (submit)
-    document.body.style.backgroundImage = `url(./img/backgrounds/${background.value})`
+const image = `url(./img/backgrounds/${background.value})`;
+    document.body.style.backgroundImage =`url(${image})`;
 })
